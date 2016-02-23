22c4ac190a863ed224542c50e7c0f550d5274ee1
Martin Litvaj <kamahl19@gmail.com>
2016-02-23 13:34:00 +0100

    Hotfix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export ReactSlides from './ReactSlides';
 export Code from './slide-components/Code';
+export Markdown from './slide-components/Markdown';
 export GoTo from './slide-components/GoTo';
 export Icon from './slide-components/Icon';
 export Link from './slide-components/Link';
