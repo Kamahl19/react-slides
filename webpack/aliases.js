ffe01df051c78812b158c8c44662c6b04ccf1ca4
Martin Litvaj <kamahl19@gmail.com>
2016-09-13 01:23:14 +0200

    no message

--- webpack/aliases.js
+++
@@ -1,7 +0,0 @@
-const resolve = require('path').resolve;
-
-const src = [__dirname, '../', 'src'];
-
-module.exports = {
-    '@': resolve(...src),
-};
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,41 +1,19 @@
-const webpack = require('webpack');
 const path = require('path');
 const yargs = require('yargs');
 const autoprefixer = require('autoprefixer');
-const aliases = require('./aliases');
 const commons = require('./common');
 
 const env = yargs.argv.mode;
 
-const libraryName = 'ReactSlides';
-
-const loaders = [
-    { test: /\.jsx?$/, loader: 'babel', include: commons.root('src') },
-    { test: /\.json$/, loader: 'json' },
-    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-    { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' }
-];
-
-let plugins;
+const loaders = [];
 
 if (env === 'build') {
-    plugins = [
-        new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
-        new webpack.optimize.UglifyJsPlugin({ minimize: true }),
-    ];
-
     loaders.push(
         { test: /\.css$/, loader: 'style!css?minimize!postcss' },
         { test: /\.less$/, loader: 'style!css?minimize!less!postcss' }
     );
 }
 else {
-    plugins = [
-        new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' }),
-    ];
-
     loaders.push(
         { test: /\.css$/, loader: 'style!css' },
         { test: /\.less$/, loader: 'style!css!less?sourceMap' }
@@ -43,12 +21,7 @@ else {
 }
 
 module.exports = {
-    entry: {
-        main: [
-            'babel-polyfill',
-            './src/main',
-        ]
-    },
+    entry: './src/main',
 
     devtool: (env === 'build') ? 'source-map' : 'cheap-module-eval-source-map',
 
@@ -56,23 +29,16 @@ module.exports = {
 
     resolve: {
         root: path.resolve('./src'),
-        extensions: ['', '.js', '.jsx', '.json', '.css', '.html', '.less'],
-        alias: aliases,
+        extensions: ['', '.js', '.jsx', '.css', '.less'],
     },
 
     output: {
         path: commons.root('lib'),
-        filename: (env === 'build') ? `${libraryName}.min.js` : `${libraryName}.js`,
-        library: (env === 'build') ? `${libraryName}.min.js` : `${libraryName}.js`,
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
     },
 
     module: {
         loaders,
     },
-
-    plugins,
 
     postcss: [
         autoprefixer({ browsers: ['last 2 versions'] })
